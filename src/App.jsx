6537c03a2ddfaad0f09b62192e4c78d28274849c
ELS <57393100+kodosa7@users.noreply.github.com>
2023-01-11 16:14:20 +0100

    code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Main from './components/Main'
 import Footer from './components/Footer'
 import './App.css'
 
+
 export default function App() {
     return (
         <div className="container">
